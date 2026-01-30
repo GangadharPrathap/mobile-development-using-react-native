760b6ed9f7311fcfd8e049cc5646d723c1429756
GangadharPrathap <23A91A0416@aec.edu.in>
2026-01-30 10:37:16 +0530

    sending

---
+++ app/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+const App = () =>{
+  return(
+
+    <Text>Prathap is the developer for this program</Text>
+  )
+}
+
+export default App
