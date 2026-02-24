c01ae1a3a33a11d1769831dfd1aa4629aaf91e1d
GangadharPrathap <23A91A0416@aec.edu.in>
2026-02-24 11:53:16 +0530

    sending the codes

--- app/App.js
+++ app/App.js
@@ -77,3 +77,196 @@ export default function App() {
    </NavigationContainer>
   );
 }
+
+
+
+
+
+
+
+
+
+
+/*
+    the navigations are quite similar as in webdevelopment we use similar Js in react native as well
+    Onclick => Onpress
+    Onchange => Onchange
+    Mostly we will be using only the Event listeners in this Applications there will be no DOM manipulations in this
+
+    buttons,text,activity listener,badges,text input ,Helper text,icons,useStates components to caputre in some variables
+    data.includes('@')
+
+
+    **check boxes**
+    which has a default status of uncheck inside if we give it as status check then it will be like selected
+    customisations:{the colors will be applied only when the check box was in check state}
+    in order make it like a button we will be using useState like;
+        status={status ? check : uncheck}
+        <button
+            mode:Contained
+            onPress : {()=> setStatus(!status)}>Click me</>
+
+        However the check box is invisible when it was used without the label in ios but in android it was visible
+
+    **Radio Button**
+
+    same as like a check box but we will be giving the status in the string format
+
+    **Switch**
+    it is a toggle switch
+    <switch
+        value = {status}   the switch can be controlled
+        onChange={()=>setStatus(!status)}
+    />
+
+
+    **Search Bar**
+    it defaultly comes with an elevated edge like a default shaded color and works like an inpput field
+    it has two modes:
+        1.View(extends the entire search bar along the screen)
+        2.bar(default case)
+    <SearchBar  />
+
+    **Chip**
+        it is like a badge format similar badges created using div (onClose functions are used only in this chips)
+    <Chip
+        Onclose:{()=>alert("closed")}
+    >My hoome</>
+
+
+    23-02-2026
+    ****** Navigation in ReactNative ********
+    there are three types of navigations ::
+    1. Top bar Navigations
+    2. Bottom bar Navigations
+    3. Stack Navigations
+    4. Drawer Navigations
+
+    ---------------------  STACK NAVIGATIONS  -------------------
+    the stack navigations as based on the stack ds format just LIFO format( When the stack becomes empty the entire application will get closed automatically)
+    Packages required :
+        @react-navigation/native
+        @react-native-screens
+        @react-native-safe-area-context         these three are mandatory to use navigations
+        react-navigation/native-stack
+
+    different screens are required in order to use the navigations
+    create Home.jsx
+    create contact.jsx
+    now we create a navigation container in order to perform the navigations
+    in order to perform any operations on the screen like titles,customisations we use screenOptions tag to do customisations in block
+    Customisations:::
+        1. Title:"   " to put a title
+        2. headerShown : True/False in order to display the header or not
+        3. headerStyle:{
+            backgroundColor:"red" to change the background colors of the header
+        }
+        4. headerTintColor: "red"  it is used to change the color of the content present in the header
+        5. headerTitleStyle:{
+            fontsize: "26"
+
+            }any kind of customizations related to font of the header we use this tag
+        6.  headerTitleAlign: "Center"
+        if we are using the native stack navigations we are not able to change the alignment of the title name in ios
+        but in the android it was possible to change the alignment
+        7. headerRight:()=><Icon source = {Bell} size = {24}>
+        this tag is used in order to place anything like icons or buttons on the header at right position
+        8. headerLeft: ()=><Icon source = {Home} size = {24}> similar to that of headerRight()
+        9. headerBackButtonDisplayMode:"minimal" then in ios screen the previous screen data won't be mentioned
+        10. animation:"" this is used to add some customised animations while we are changing in between the screens
+
+
+
+    ---------------- BOTTOM TAB NAVIGATIONS --------------------
+    best example for the bottom tab navigations is instagram
+    import the naviagtion container from the react-naviagtion/native
+    import tthe createbottomtabnavigator from the @react-navigation/bottom-tab
+    const BottomTabNavigator = createbottomtabnavigator()
+    <NavigationContainer>
+      <BottomTabNavigation.Navigator>
+        <BottomTabNavigation.Screen name = "Home" component = {Home}/>
+        <BottomTabNavigation.Screen name = "Contact" component = {Contact}/>
+      </>
+    </>
+
+    :::::Customizations:::::
+    the customisations will be applied for the entire navigations or can be applied for individual as well
+    screenOptions={}
+    1. TabBarActiveTintcolor: used in order to change the color active components
+    2. TabBarInActiveTintcolor: used in order to change the color inactive components
+    3. tabBarLabelStyle: this is used in order to change the font related stylings for the labels that are mentioned
+        1. fontSize: change the label font size
+        2. borderWidth: add some border only for the text labels
+    4. tabBarItemStyle: the tab bar items are the no of blocks that are present in the bottom bar like: home block and contact block
+    in order to do some stylings we use tabBarItemStyles
+          we use tags like height,width
+    5. tabBarIcon: it is used in order to customise the icon in the tabs now we follow this procedure in order to put dynamic icons
+      screenOptions={({route})=>{
+        tabBarIcon:({})=>{
+          if(route.name == 'Home')      while using this format the icon won't be able to change its state whether it is acitve or not
+          return<Icon source ={"home"}/>
+          if(route.name == 'Contact')
+          return<Icon source ={"car"}/>
+          }
+        }}
+
+
+        tabBarIcon:({focused,color})=>{
+          let IconName;
+          if(route.name == "Home"){
+              IconName = focused ? "Home" : "home-outline"
+          }
+          if(route.name == "Contact"){
+              IconName = focused ? "contact" : "contact-outline"
+          }
+          return<Icon source ={IconName} size={30} color={color}/>
+          },
+          tabbaractivetintcolor = "green",
+          tabBarActiveTintColor = "red"
+        }}
+
+    ------------------ STACK + BOTTOM NAVIGATIONS-----------------
+    the tabnavigation componenet will be having the entire code for the bottom tab navigations now in app.js we use
+    const Stack = createNativeStackNavigator();
+    import profile
+    import tabNaviagtions
+    <NavigationContainer> only one should be there so remove it in the TabNavigation
+      <Stack.Navigator>
+        <Stack.Screen name = "TabNavigation" component = {TabNavigation}/>
+        <Stack.Screen name = "Profile" component = {profile}/>
+
+        in contact page
+        we will add a button to change the page from the contact page to the profile page
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
